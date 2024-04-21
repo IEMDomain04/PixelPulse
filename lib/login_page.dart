adffe5a72ecd56407a79e5688ecd4d4b51896f50
IEMDomain04.dc <emmanman0011@gmail.com>
2024-04-21 23:38:45 +0800

    Random Login muna

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              Icons.account_circle,
+              size: 100.0,
+              color: Colors.blue,
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Email or Username',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                // Add login functionality here
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
